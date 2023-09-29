a1cf4a856939f83c701ec694f405ba0270623df7
Tuyen Bui <mnit012899@gmail.com>
2023-09-29 18:45:36 +0700

    update admin

--- public/admin/index.php
+++ public/admin/index.php
@@ -34,218 +34,221 @@ if (!isset($_SESSION['CREATED'])) {
     <link rel="shortcut icon" href="../icon/logo-web.png" />
     <link rel="stylesheet" href="../css/s.css" />
 </head>
-<body class="container">
-    <div class="position-fixed w-100 background-grey" style="top: 0; right:0">
-        <div class="container">
-            <a class="float-right" href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+<body>
+    <div class="container mt-5">
+        <div class="position-fixed w-100 background-grey" style="top: 0; right:0">
+            <div class="container">
+                <a class="float-right" href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+            </div>
         </div>
-    </div>
-    <div>
-        <h1>Contents</h1>
-        <?php
-        foreach ($fetchData as $e) { ?>
-            
-        <div class="mb-3 mt-5">
-            <form action="../actions/save_content.php" method="post">
-                <h2><?php echo "$e[name]"?></h2>
-                <input type="text" name="name" hidden value="<?php echo $e["name"] ?>">
-                <textarea class="w-100 smaller" rows="5" type="text" name="content"><?php echo str_replace("<p>","\n",$e["content"]); ?></textarea>
-                <input type="submit" value="Save" name="submit">
-            </form>
-        </div>
-        <hr/>
-        <?php  
-        }
-        ?>
-        
-    </div>
-    <div>
-        <h1>Images part</h1>
-        <?php
-        foreach ($home_main as $e) { ?>
+        <div class="row">
+        <div class="col-6">
+            <h1>Contents</h1>
+            <?php
+            foreach ($fetchData as $e) { ?>
+                
             <div class="mb-3 mt-5">
-            <form action="../actions/upload.php" method="post" enctype="multipart/form-data">
-                <h3><?php echo "$e[name] - $e[des]"?></h3>
-                <input type="text" name="fileName" hidden value="<?php echo $e["path"] ?>">
-                <input type="file" name="fileToUpload" id="fileToUpload">
-                <input type="submit" value="Upload" name="submit">
-            </form>
-        </div>  
-        <hr/>
-        <?php  
-        }
-        ?>
-        
-        <?php
-        foreach ($why_euro as $e) { ?>
-        <div class="mb-3 mt-5">
-            <form action="../actions/upload.php" method="post" enctype="multipart/form-data">
-                <h3><?php echo "$e[name] - $e[des]"?></h3>
-                <input type="text" name="fileName" hidden value="<?php echo $e["path"] ?>">
-                <input type="file" name="fileToUpload" id="fileToUpload">
-                <input type="submit" value="Upload" name="submit">
-            </form>
-        </div> 
-        <?php
-        }
-        ?>
-        <hr/>
-        <?php
-        foreach ($intro_product as $e) { ?>
-        <div class="mb-3 mt-5">
-            <form action="../actions/upload.php" method="post" enctype="multipart/form-data">
-                <h3><?php echo "$e[name] - $e[des]"?></h3>
-                <input type="text" name="fileName" hidden value="<?php echo $e["path"] ?>">
-                <input type="file" name="fileToUpload" id="fileToUpload">
-                <input type="submit" value="Upload" name="submit">
-            </form>
-        </div> 
-        <?php
-        }
-        ?>
-        <hr/>
-        <?php
-        foreach ($intro_facility as $e) { ?>
-        <div class="mb-3 mt-5">
-            <form action="../actions/upload.php" method="post" enctype="multipart/form-data">
-                <h3><?php echo "$e[name] - $e[des]"?></h3>
-                <input type="text" name="fileName" hidden value="<?php echo $e["path"] ?>">
-                <input type="file" name="fileToUpload" id="fileToUpload">
-                <input type="submit" value="Upload" name="submit">
-            </form>
-        </div> 
-        <?php
-        }
-        ?>
-        <hr/>
-        <?php
-        foreach ($partner as $e) { ?>
-        <div class="mb-3 mt-5">
-            <form action="../actions/upload.php" method="post" enctype="multipart/form-data">
-                <h3><?php echo "$e[name] - $e[des]"?></h3>
-                <input type="text" name="fileName" hidden value="<?php echo $e["path"] ?>">
-                <input type="file" name="fileToUpload" id="fileToUpload">
-                <input type="submit" value="Upload" name="submit">
-            </form>
-        </div> 
-        <?php
-        }
-        ?>
-        <hr/>
-        <?php
-        foreach ($product_main as $e) { ?>
-        <div class="mb-3 mt-5">
-            <form action="../actions/upload.php" method="post" enctype="multipart/form-data">
-                <h3><?php echo "$e[name] - $e[des]"?></h3>
-                <input type="text" name="fileName" hidden value="<?php echo $e["path"] ?>">
-                <input type="file" name="fileToUpload" id="fileToUpload">
-                <input type="submit" value="Upload" name="submit">
-            </form>
-        </div> 
-        <?php
-        }
-        ?>
-        <hr/>
-        <?php
-        foreach ($product as $e) { ?>
-        <div class="mb-3 mt-5">
-            <form action="../actions/upload.php" method="post" enctype="multipart/form-data">
-                <h3><?php echo "$e[name] - $e[des]"?></h3>
-                <input type="text" name="fileName" hidden value="<?php echo $e["path"] ?>">
-                <input type="file" name="fileToUpload" id="fileToUpload">
-                <input type="submit" value="Upload" name="submit">
-            </form>
-        </div> 
-        <?php
-        }
-        ?>
-        <hr/>
-        <?php
-        foreach ($lab_main as $e) { ?>
-        <div class="mb-3 mt-5">
-            <form action="../actions/upload.php" method="post" enctype="multipart/form-data">
-                <h3><?php echo "$e[name] - $e[des]"?></h3>
-                <input type="text" name="fileName" hidden value="<?php echo $e["path"] ?>">
-                <input type="file" name="fileToUpload" id="fileToUpload">
-                <input type="submit" value="Upload" name="submit">
-            </form>
-        </div> 
-        <?php
-        }
-        ?>
-        <hr/>
-        <?php
-        foreach ($lab as $e) { ?>
-        <div class="mb-3 mt-5">
-            <form action="../actions/upload.php" method="post" enctype="multipart/form-data">
-                <h3><?php echo "$e[name] - $e[des]"?></h3>
-                <input type="text" name="fileName" hidden value="<?php echo $e["path"] ?>">
-                <input type="file" name="fileToUpload" id="fileToUpload">
-                <input type="submit" value="Upload" name="submit">
-            </form>
-        </div> 
-        <?php
-        }
-        ?>
-        <hr/>
-        <?php
-        foreach ($infra_main as $e) { ?>
-        <div class="mb-3 mt-5">
-            <form action="../actions/upload.php" method="post" enctype="multipart/form-data">
-                <h3><?php echo "$e[name] - $e[des]"?></h3>
-                <input type="text" name="fileName" hidden value="<?php echo $e["path"] ?>">
-                <input type="file" name="fileToUpload" id="fileToUpload">
-                <input type="submit" value="Upload" name="submit">
-            </form>
-        </div> 
-        <?php
-        }
-        ?>
-        <hr/>
-        <?php
-        foreach ($infra as $e) { ?>
-        <div class="mb-3 mt-5">
-            <form action="../actions/upload.php" method="post" enctype="multipart/form-data">
-                <h3><?php echo "$e[name] - $e[des]"?></h3>
-                <input type="text" name="fileName" hidden value="<?php echo $e["path"] ?>">
-                <input type="file" name="fileToUpload" id="fileToUpload">
-                <input type="submit" value="Upload" name="submit">
-            </form>
-        </div> 
-        <?php
-        }
-        ?>
-        <hr/>
-        <?php
-        foreach ($contact_main as $e) { ?>
-        <div class="mb-3 mt-5">
-            <form action="../actions/upload.php" method="post" enctype="multipart/form-data">
-                <h3><?php echo "$e[name] - $e[des]"?></h3>
-                <input type="text" name="fileName" hidden value="<?php echo $e["path"] ?>">
-                <input type="file" name="fileToUpload" id="fileToUpload">
-                <input type="submit" value="Upload" name="submit">
-            </form>
-        </div> 
-        <?php
-        }
-        ?>
-        <hr/>
-        <?php
-        foreach ($contact as $e) { ?>
-        <div class="mb-3 mt-5">
-            <form action="../actions/upload.php" method="post" enctype="multipart/form-data">
-                <h3><?php echo "$e[name] - $e[des]"?></h3>
-                <input type="text" name="fileName" hidden value="<?php echo $e["path"] ?>">
-                <input type="file" name="fileToUpload" id="fileToUpload">
-                <input type="submit" value="Upload" name="submit">
-            </form>
-        </div> 
-        <?php
-        }
-        ?>
-        <hr>
+                <form action="../actions/save_content.php" method="post">
+                    <h2><?php echo "$e[name]"?></h2>
+                    <input type="text" name="name" hidden value="<?php echo $e["name"] ?>">
+                    <textarea class="w-100 smaller" rows="5" type="text" name="content"><?php echo str_replace("<p>","\n",$e["content"]); ?></textarea>
+                    <input type="submit" value="Save" name="submit">
+                </form>
+            </div>
+            <hr/>
+            <?php  
+            }
+            ?>
+            
+        </div>
+        <div class="col-6">
+            <h1>Images part</h1>
+            <?php
+            foreach ($home_main as $e) { ?>
+                <div class="mb-3 mt-5">
+                <form action="../actions/upload.php" method="post" enctype="multipart/form-data">
+                    <h3><?php echo "$e[name] - $e[des]"?></h3>
+                    <input type="text" name="fileName" hidden value="<?php echo $e["path"] ?>">
+                    <input type="file" name="fileToUpload" id="fileToUpload">
+                    <input type="submit" value="Upload" name="submit">
+                </form>
+            </div>  
+            <hr/>
+            <?php  
+            }
+            ?>
+            
+            <?php
+            foreach ($why_euro as $e) { ?>
+            <div class="mb-3 mt-5">
+                <form action="../actions/upload.php" method="post" enctype="multipart/form-data">
+                    <h3><?php echo "$e[name] - $e[des]"?></h3>
+                    <input type="text" name="fileName" hidden value="<?php echo $e["path"] ?>">
+                    <input type="file" name="fileToUpload" id="fileToUpload">
+                    <input type="submit" value="Upload" name="submit">
+                </form>
+            </div> 
+            <?php
+            }
+            ?>
+            <hr/>
+            <?php
+            foreach ($intro_product as $e) { ?>
+            <div class="mb-3 mt-5">
+                <form action="../actions/upload.php" method="post" enctype="multipart/form-data">
+                    <h3><?php echo "$e[name] - $e[des]"?></h3>
+                    <input type="text" name="fileName" hidden value="<?php echo $e["path"] ?>">
+                    <input type="file" name="fileToUpload" id="fileToUpload">
+                    <input type="submit" value="Upload" name="submit">
+                </form>
+            </div> 
+            <?php
+            }
+            ?>
+            <hr/>
+            <?php
+            foreach ($intro_facility as $e) { ?>
+            <div class="mb-3 mt-5">
+                <form action="../actions/upload.php" method="post" enctype="multipart/form-data">
+                    <h3><?php echo "$e[name] - $e[des]"?></h3>
+                    <input type="text" name="fileName" hidden value="<?php echo $e["path"] ?>">
+                    <input type="file" name="fileToUpload" id="fileToUpload">
+                    <input type="submit" value="Upload" name="submit">
+                </form>
+            </div> 
+            <?php
+            }
+            ?>
+            <hr/>
+            <?php
+            foreach ($partner as $e) { ?>
+            <div class="mb-3 mt-5">
+                <form action="../actions/upload.php" method="post" enctype="multipart/form-data">
+                    <h3><?php echo "$e[name] - $e[des]"?></h3>
+                    <input type="text" name="fileName" hidden value="<?php echo $e["path"] ?>">
+                    <input type="file" name="fileToUpload" id="fileToUpload">
+                    <input type="submit" value="Upload" name="submit">
+                </form>
+            </div> 
+            <?php
+            }
+            ?>
+            <hr/>
+            <?php
+            foreach ($product_main as $e) { ?>
+            <div class="mb-3 mt-5">
+                <form action="../actions/upload.php" method="post" enctype="multipart/form-data">
+                    <h3><?php echo "$e[name] - $e[des]"?></h3>
+                    <input type="text" name="fileName" hidden value="<?php echo $e["path"] ?>">
+                    <input type="file" name="fileToUpload" id="fileToUpload">
+                    <input type="submit" value="Upload" name="submit">
+                </form>
+            </div> 
+            <?php
+            }
+            ?>
+            <hr/>
+            <?php
+            foreach ($product as $e) { ?>
+            <div class="mb-3 mt-5">
+                <form action="../actions/upload.php" method="post" enctype="multipart/form-data">
+                    <h3><?php echo "$e[name] - $e[des]"?></h3>
+                    <input type="text" name="fileName" hidden value="<?php echo $e["path"] ?>">
+                    <input type="file" name="fileToUpload" id="fileToUpload">
+                    <input type="submit" value="Upload" name="submit">
+                </form>
+            </div> 
+            <?php
+            }
+            ?>
+            <hr/>
+            <?php
+            foreach ($lab_main as $e) { ?>
+            <div class="mb-3 mt-5">
+                <form action="../actions/upload.php" method="post" enctype="multipart/form-data">
+                    <h3><?php echo "$e[name] - $e[des]"?></h3>
+                    <input type="text" name="fileName" hidden value="<?php echo $e["path"] ?>">
+                    <input type="file" name="fileToUpload" id="fileToUpload">
+                    <input type="submit" value="Upload" name="submit">
+                </form>
+            </div> 
+            <?php
+            }
+            ?>
+            <hr/>
+            <?php
+            foreach ($lab as $e) { ?>
+            <div class="mb-3 mt-5">
+                <form action="../actions/upload.php" method="post" enctype="multipart/form-data">
+                    <h3><?php echo "$e[name] - $e[des]"?></h3>
+                    <input type="text" name="fileName" hidden value="<?php echo $e["path"] ?>">
+                    <input type="file" name="fileToUpload" id="fileToUpload">
+                    <input type="submit" value="Upload" name="submit">
+                </form>
+            </div> 
+            <?php
+            }
+            ?>
+            <hr/>
+            <?php
+            foreach ($infra_main as $e) { ?>
+            <div class="mb-3 mt-5">
+                <form action="../actions/upload.php" method="post" enctype="multipart/form-data">
+                    <h3><?php echo "$e[name] - $e[des]"?></h3>
+                    <input type="text" name="fileName" hidden value="<?php echo $e["path"] ?>">
+                    <input type="file" name="fileToUpload" id="fileToUpload">
+                    <input type="submit" value="Upload" name="submit">
+                </form>
+            </div> 
+            <?php
+            }
+            ?>
+            <hr/>
+            <?php
+            foreach ($infra as $e) { ?>
+            <div class="mb-3 mt-5">
+                <form action="../actions/upload.php" method="post" enctype="multipart/form-data">
+                    <h3><?php echo "$e[name] - $e[des]"?></h3>
+                    <input type="text" name="fileName" hidden value="<?php echo $e["path"] ?>">
+                    <input type="file" name="fileToUpload" id="fileToUpload">
+                    <input type="submit" value="Upload" name="submit">
+                </form>
+            </div> 
+            <?php
+            }
+            ?>
+            <hr/>
+            <?php
+            foreach ($contact_main as $e) { ?>
+            <div class="mb-3 mt-5">
+                <form action="../actions/upload.php" method="post" enctype="multipart/form-data">
+                    <h3><?php echo "$e[name] - $e[des]"?></h3>
+                    <input type="text" name="fileName" hidden value="<?php echo $e["path"] ?>">
+                    <input type="file" name="fileToUpload" id="fileToUpload">
+                    <input type="submit" value="Upload" name="submit">
+                </form>
+            </div> 
+            <?php
+            }
+            ?>
+            <hr/>
+            <?php
+            foreach ($contact as $e) { ?>
+            <div class="mb-3 mt-5">
+                <form action="../actions/upload.php" method="post" enctype="multipart/form-data">
+                    <h3><?php echo "$e[name] - $e[des]"?></h3>
+                    <input type="text" name="fileName" hidden value="<?php echo $e["path"] ?>">
+                    <input type="file" name="fileToUpload" id="fileToUpload">
+                    <input type="submit" value="Upload" name="submit">
+                </form>
+            </div> 
+            <?php
+            }
+            ?>
+            <hr>
+        </div>
+        </div>
     </div>
-
 </body>
 <script src="https://kit.fontawesome.com/4e75f15372.js" crossorigin="anonymous"></script>
     <script src="../lib/js/jquery.min.js"></script>
