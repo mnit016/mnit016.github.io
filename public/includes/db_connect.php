3b659db652e0f363d69aa5808e53e3ad3aa12da9
Tuyen Bui <mnit012899@gmail.com>
2023-09-29 12:23:26 +0700

    update db credential

--- public/includes/db_connect.php
+++ public/includes/db_connect.php
@@ -2,10 +2,10 @@
 session_start();
 
 $db_host = "localhost";
-// $db_user = "eurofilm_admin";
-// $db_pass = "sgC2MGv437Bu";
-$db_user = "root";
-$db_pass = "";
+$db_user = "eurofilm_admin";
+$db_pass = "sgC2MGv437Bu";
+// $db_user = "root";
+// $db_pass = "";
 $db_name = "eurofilm_main";
 
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
