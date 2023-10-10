6696174e5b78ff0a244d523a84f8f200cb7417cd
Tuyen Bui <mnit012899@gmail.com>
2023-10-10 22:35:29 +0700

    feature add/delete image for why-euro-slide

---
+++ public/actions/update_home_video.php
@@ -0,0 +1,15 @@
+<?php
+include("../includes/db_connect.php");
+
+$query= "UPDATE img SET path='$_POST[url]' WHERE id='$_POST[id]'";
+$result = $conn->query($query);
+if ($result== true) { 
+    $msg="Updated successfully";
+} else {
+    $msg=mysqli_error($conn);
+}
+if (session_id() == "")
+    session_start();
+$_SESSION["msg"] = "$msg";
+header("Location: /admin/");
+?>
