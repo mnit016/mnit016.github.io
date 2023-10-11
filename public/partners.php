6ac7d8c64c6b37133e1c58ef8d6077422c4a99ce
Tuyen Bui <mnit012899@gmail.com>
2023-10-11 12:30:21 +0700

    update partners

--- public/partners.php
+++ public/partners.php
@@ -7,80 +7,24 @@
                     <div class="col-12 p-0">
                         <div id="doitac-slide" class="carousel slide" data-ride="carousel">
                             <div class="carousel-inner">
-                            <?php
-                                for ($i=0; $i < count($why_euro); $i++) { ?>
-                                <div class="carousel-item <?php if ($i == 0) { ?>active <?php } ?>">
+                                <div class="carousel-item active">
                                     <ul class="row img-sli justify-content-between">
+                                    <?php
+                                    for ($i=0; $i < count($partner); $i++) { ?>
                                         <li class="">
-                                            <img src="../images/doitac/ampacking copy-01.png" alt="">
+                                            <img src="../images/<?php echo $partner[$i]["path"] ?>" alt="">
                                         </li>
-                                        <li class="">
-                                            <img src="../images/doitac/bao bi trung son copy.png" alt="">
-                                        </li>
-                                        <li class="">
-                                            <img src="../images/doitac/bao tin copy-01-01.png" alt="">
-                                        </li>
-                                        <li class="">
-                                            <img src="../images/doitac/batico.png" alt="">
-                                        </li>
-                                        <li class="">
-                                            <img src="../images/doitac/bunzl.jpg" alt="">
-                                        </li>
-                                        <li class="">
-                                            <img src="../images/doitac/dai luc copy.png" alt="">
-                                        </li>
-                                    </ul>
-                                </div>
-                            <?php } ?>
-                                <div class="carousel-item">
-                                    <ul class="row img-sli justify-content-between">
-                                        <li class="">
-                                            <img src="../images/doitac/dica.png" alt="">
-                                        </li>
-                                        <li class="">
-                                            <img src="../images/doitac/goda.png" alt="">
-                                        </li>
-                                        <li class="">
-                                            <img src="../images/doitac/lo duc.png" alt="">
-                                        </li>
-                                        <li class="">
-                                            <img src="../images/doitac/Logo-HMT-V-01 copy.png" alt="">
-                                        </li>
-                                        <li class="">
-                                            <img src="../images/doitac/logo1.png" alt="">
-                                        </li>
-                                        <li class="">
-                                            <img src="../images/doitac/saphia copy.png" alt="">
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="carousel-item">
-                                    <ul class="row img-sli justify-content-between">
-                                        <li class="">
-                                            <img src="../images/doitac/tan dai hung.png" alt="">
-                                        </li>
-                                        <li class="">
-                                            <img src="../images/doitac/tan hiep loi.png" alt="">
-                                        </li>
-                                        <li class="">
-                                            <img src="../images/doitac/tien phat.png" alt="">
-                                        </li>
-                                        <li class="">
-                                            <img src="../images/doitac/viet hoa.png" alt="">
-                                        </li>
-                                        <li class="">
-                                            <img src="../images/doitac/vinapacking.png" alt="">
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="carousel-item">
-                                    <ul class="row img-sli justify-content-between">
-                                        <li class="col-6">
-                                            <img src="../images/ctlk/1.png" alt="">
-                                        </li>
-                                        <li class="col-6">
-                                            <img src="../images/ctlk/2.png" alt="">
-                                        </li>
+                                        <?php 
+                                        if ($i > 0 && count($partner) > $i + 1 && $i % 6 == 0) {
+                                        ?>
+                                        </ul>
+                                        </div>
+                                        <div class="carousel-item">
+                                        <ul class="row img-sli justify-content-between">        
+                                        <?php } ?>
+                                    <?php 
+                                    } 
+                                    ?>
                                     </ul>
                                 </div>
                             </div>
