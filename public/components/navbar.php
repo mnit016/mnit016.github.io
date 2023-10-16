65d970d7d0a495f03f88834a8b71c275aa27e597
Tuyen Bui <mnit012899@gmail.com>
2023-10-16 11:39:46 +0700

    update nav bar

--- public/components/navbar.php
+++ public/components/navbar.php
@@ -1,11 +1,11 @@
-<nav class="euro-font-nav sticky-top w-100 background-grey pt-2 pb-2" style="font-size: 1.1rem;">
+<nav class="euro-font-nav sticky-top w-100 background-image pt-2 pb-2" style="font-size: 1rem; background-image: url(./images/nav/nav-bg.JPG);">
     <div class="container-xl p-0">
         <div class="font-weight-bold">
             <div class="row col-12 p-0">
                 <div class="col-12 d-flex justify-content-between">
                     <a class="nav-link p-0" href="#index"><img src="./icon/logo-15.png" alt=""></a>
                     <div class="d-flex">
-                        <ul class="d-flex align-items-center mb-0 text-red">
+                        <ul class="d-flex align-items-center mb-0 text-white">
                             <li class="">
                                 <a class="nav-link" href="#about-us"><?= _NAV_ABOUT_US ?><span class="sr-only">(current)</span></a>
                             </li>
@@ -34,8 +34,8 @@
 
                         <form class="form-inline my-2 my-lg-0">
                             <div class="d-flex align-items-center position-relative ml-2">
-                                <input class="form-control mr-sm-2 pl-4 background-grey" type="search" style="width: 170px" aria-label="Search" />
-                                <i class="fa-solid fa-magnifying-glass position-absolute text-red" style="left: 0.5rem;"></i>
+                                <input class="form-control mr-sm-2 pl-4 text-white" type="search" style="width: 170px; background: transparent;" aria-label="Search" />
+                                <i class="fa-solid fa-magnifying-glass position-absolute text-white" style="left: 0.5rem;"></i>
                             </div>
                             <div class="d-flex align-items-center">
                                 <a class="euro-font-nav nav-link p-0 dropdown-icon-after" href="/swap_lang.php?redirect_page=/"><?= LANG ?></a>
